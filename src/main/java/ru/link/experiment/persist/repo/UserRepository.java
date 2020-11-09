71d20a6cbee1b1e1504935fd8e2febeaa6472e2d
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2020-11-09 18:30:09 +0700

    Clean up workspace

--- src/main/java/ru/link/experiment/persist/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.link.experiment.persist.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.link.experiment.persist.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
